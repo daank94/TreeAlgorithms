4ddd6442b2aec0c2170c58be08ce03404f39ba33
Daan Kolthof <daan_kolthof@hotmail.com>
2019-12-25 22:22:26 +0100

    Treenode file containing binary tree Node class to use in algorithms.

---
+++ treenode.py
@@ -0,0 +1,9 @@
+class Node:
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __repr__(self) -> str:
+        return "Node [value: '" + str(self.value) + "', left: " + str(self.left) + ", right: " + str(self.right) + "]"
